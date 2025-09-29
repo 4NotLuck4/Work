cb39450c4f7da93ae717561ac82bc4127827160d
GSEvgenievich <Stepan.geras2006@yandex.ru>
2025-09-29 12:53:57 +0300

    Р”РѕР±Р°РІСЊС‚Рµ С„Р°Р№Р»С‹ РїСЂРѕРµРєС‚Р°.

---
+++ LabWork7/DataAccessLayer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace LabWork7
+{
+    public static class DataAccessLayer
+    {
+        //5.1
+        //5.1.1
+        private static string _server = "mssql";
+        private static string _datebase = "ispp3102";
+        private static string _login = "ispp3102";
+        private static string _password = "3102";
+
+        public static string ConnectionString
+        {
+            get
+            {
+                var bulder = new SqlConnectionStringBuilder
+                {
+                    DataSource = _server,
+                    InitialCatalog = _datebase,
+                    UserID = _login,
+                    Password = _password,
+                    TrustServerCertificate = true
+                };
+                return bulder.ConnectionString;
+            }
+        }
+
+        //5.1.2
+        public static void ChangeConnectionSetting(string server, string database, string login, string password)
+        {
+            _server = server;
+            _datebase = database;
+            _login = login;
+            _password = password;
+        }
+
+        //5.1.3
+        public static bool TestConnection()
+        {
+            try
+            {
+                using var connection = new SqlConnection(ConnectionString);
+                connection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //5.2
+        //5.2.1
+        public static async Task<int> ExecuteNonQueryAsync(string sqlCommand)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand(sqlCommand, connection))
+                {
+                    return await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
+    }
+}
---
+++ LabWork7/Program.cs
@@ -0,0 +1,14 @@
+﻿using LabWork7;
+
+//5.1.1
+Console.WriteLine("Исходная строка подключения:");
+Console.WriteLine(DataAccessLayer.ConnectionString);
+
+//5.1.2
+DataAccessLayer.ChangeConnectionSetting("NEW_SERVER", "NEW_BD", "admin", "newpass");
+Console.WriteLine("Новая строка подключения:");
+Console.WriteLine(DataAccessLayer.ConnectionString);
+
+//5.1.3
+bool canConnect = DataAccessLayer.TestConnection();
+Console.WriteLine($"Подключение к БД: {(canConnect ? "УСПЕШНО" : "ОШИБКА")}");
