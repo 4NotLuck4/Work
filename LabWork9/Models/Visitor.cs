ae6d8a65118969cf5c220ec5ab1c2933510ef726
Виктор Терентьев <viktorterentevPC@gmail.com>
2025-10-07 22:48:41 +0300

    Lb9

---
+++ LabWork9/Models/Visitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork9.Models
+{
+    [Table("Visitor")]
+    public class Visitor
+    {
+        public int VisitorId { get; set; }
+        public string Name { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+    }
+}
