87308182600108a063ea531850e7cf5571584b12
Виктор Терентьев <viktorterentevPC@gmail.com>
2025-10-15 21:31:20 +0300

    11

--- LabWork11/MainWindow.xaml.cs
+++ LabWork11/MainWindow.xaml.cs
@@ -1,54 +1,24 @@
-﻿using LabWork11.Contexts;
-using LabWork11.Services;
-using System.Collections.ObjectModel;
+﻿using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace LabWork11
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly FilmService _filmService;
-
-        private ObservableCollection<Models.Film> _films;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            var context = new AppDbContext();
-            context.Database.EnsureCreated();
-            _filmService = new FilmService(context);
-
-            LoadFilms();
-        }
-
-        private void LoadFilms()
-        {
-            _films = new ObservableCollection<Models.Film>((IEnumerable<Models.Film>)_filmService.GetFilms());
-            FilmsDataGrid.ItemsSource = _films;
-        }
-
-        private void DeleteButton_Click(object sender, RoutedEventArgs e)
-        {
-            var selectedFilms = FilmsDataGrid.SelectedItems.Cast<Models.Film>().ToList();
-            if (selectedFilms.Count == 0)
-            {
-                MessageBox.Show("Выберите фильмы для удаления", "Информация",
-                    MessageBoxButton.OK, MessageBoxImage.Information);
-                return;
-            }
-
-            try
-            {
-                _filmService.DeleteFilms(selectedFilms);
-
-                LoadFilms();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка подключения: {ex.Message}", "Ошибка", 
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
         }
     }
 }
